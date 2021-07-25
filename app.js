2cb1c3a8b5a8ed0778dad5a3e96894270393cae5
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-07-25 21:39:56 +0530

    events module

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var logger = require("./logger");
-logger.log("Loading success :))))");
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+// Register a listener
+emitter.on("messageLogged", () => {
+  console.log("listened");
+});
+
+// Raise an event
+emitter.emit("messageLogged");
